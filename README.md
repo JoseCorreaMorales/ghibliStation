99723947b6b017412e914482fecd4e4f5de6c18a
JoseCorreaMorales <jose.correax@gmail.com>
2023-12-11 12:36:59 -0600

    getting login values

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # react-login-practice
  
+## description
+This is a react + vite practice project for a login page using pico.css framework.
+
+
+## Reference
+- [pico.css](https://picocss.com/)
+
+
--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import '@picocss/pico'
 import './style/theme.css'
 import './App.css'
@@ -7,31 +8,51 @@ import loginImg from './assets/loginImg.svg'
 const Image = () => {
   return (
     <div className='img-container'>
-      <img src={loginImg} alt="login image" srcset="" />
+      <img src={loginImg} alt="login image" />
     </div>
   )
 }
 
 
-const LoginInput = () => {
+const LoginInput = ({ onFormSubmit }) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleEmailChange = ({ target: { value } }) => {
+    setUsername(value);
+  }
+  const handlePasswordChange = ({ target: { value } }) => {
+    setPassword(value);
+  }
+
+  const handleFormSubmit = (e) => { 
+      //e.preventDefault();
+      onFormSubmit(username, password);
+      
+   }
+
   return (
     <div className='input-container'>
       <label htmlFor="username">Username</label>
-      <input type="text" name='username' placeholder='username' />
+      <input type="email" name='username' placeholder='username' onChange={ handleEmailChange } required />
       <label htmlFor="password">Password</label>
-      <input type="password" name='password' placeholder='password' />
-      <button>Login </button>
+      <input type="password" name='password' placeholder='password' onChange={ handlePasswordChange } required />
+      <button type='submit' onClick={ handleFormSubmit }>Login </button>
     </div>
   )
 }
 
 function App() {
+  const handleFormSubmit = (username, password) => {    
+    console.log("data ", username, password);
+  }
+
   return (
     <>
-      <form action="">
+      <form action="" onSubmit={ (e) => e.preventDefault() }>
         <div data-theme="dark" className='container container-flex'>
 
-          <LoginInput />
+          <LoginInput onFormSubmit = { handleFormSubmit } />
 
           <Image />
 
