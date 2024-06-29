a7ae1ac204c9230084cfe527a8d707bcdbc2c5c8
JoseCorreaMorales <jose.correax@gmail.com>
2024-06-29 15:11:58 -0600

    movies cards maquetation

--- src/components/ghibliHome.jsx
+++ src/components/ghibliHome.jsx
@@ -1,14 +1,21 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import "../style/home.css";
+import { FaStar } from 'react-icons/fa'
+import { FaClock } from "react-icons/fa";
 
 const GhibliHome = () => {
-    const [films, setFilms] = useState()
+    const [films, setFilms] = useState();
+    const [loading, setLoading] = useState(true);
     useEffect(() => {
         async function fetchFilms() {
             try {
-                const response = await axios.get(import.meta.env.VITE_BASE_URL + `/films`);
+                const response = await axios.get(
+                    import.meta.env.VITE_BASE_URL + `/films`
+                );
                 console.log(response.data);
-                setFilms(response.data) 
+                setFilms(response.data);
+                setLoading(false);
             } catch (error) {
                 console.error("Error fetching data: ", error);
             }
@@ -16,17 +23,51 @@ const GhibliHome = () => {
         fetchFilms();
     }, []);
 
+    if (loading) {
+        return (
+            <div className="loading">
+                <span aria-busy={loading} className="">
+                    Loading films ✨✨...
+                </span>
+            </div>
+        );
+    }
     return (
         <>
-            {films?.map((film) => {
-                return (                    
-                    <article>{film.title}</article>
-                )
-            })}
-            <div>We are in GhibliHome</div>
+            <main className="container-fluid">
+                <div>We are in GhibliHome</div>
+                <div className="films">
+                    {films?.map((film) => {
+                        return (
+                            <article className="film" key={film.id}>
+                                <header>
+                                    <img src={film.image} alt=""  />
+                                </header>
+                                <hgroup>
+                                    <h3>{film.title}</h3>
+                                    <p>{film.original_title}</p>
+                                    <blockquote>
+                                        {film.description}                                        
+                                    </blockquote> 
+                                </hgroup>
+                                <footer>
+                                    <div className="footer-container">
+                                        <div className="footer-item">
+                                            < FaStar className="start" />
+                                            {film.rt_score}</div>
+                                        <div className="footer-item">
+                                            < FaClock className="clock" />
+                                            {film.running_time } m
+                                        </div>
+                                    </div>
+                                </footer>
+                            </article>
+                        );
+                    })}
+                </div>
+            </main>
         </>
     );
-
 };
 
 export default GhibliHome;
